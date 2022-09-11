9d07099397ceab74cec6a44349d43beb8f8a92e1
jac11 <jac11devel@gmail.com>
2022-09-11 07:51:08 -0400

    Add files via upload

--- MixList.py
+++ MixList.py
@@ -1,32 +1,33 @@
 #!/sur/bin/env python
 import os
 import sys
-
+import json
 import argparse
 with  open ('Banner','r') as readbanner:
       banner = readbanner.read()
       print(banner)    
 class Gen_List:
-     def __init__(self):                        
+     def __init__(self):   
+                         
          self.control()
-         self.List_Gen()         
+         self.List_Gen()  
+         self.JSON_FORMAT()       
      def List_Gen(self): 
-          path = self.args.Outlist+'_Mix_pass'
-          path2 = self.args.Outlist+'_Mix_UserName'
-          if os.path.exists(path) and os.path.exists(path2) :
-             os.remove(path)
-             os.remove(path2)
-             with open (path,'w') as List :
-                  with open (path2,'w') as List :
+          self.path = self.args.Outlist+'_Mix_pass'
+          self.path2 = self.args.Outlist+'_Mix_UserName'         
+          if os.path.exists(self.path) and os.path.exists(self.path2) :
+             os.remove(self.path)
+             os.remove(self.path2)
+             with open (self.path,'w') as List :
+                  with open (self.path2,'w') as List :
                        pass
           Count = 0                         
           for  i in range(int(self.args.Repete/3+1)):
-               with open (path2,'a') as List :
+               with open (self.path2,'a') as List :
                     if Count ==  int(self.args.Repete/3+1) :
                         break
                     else:   
-                        MixUserName = List.write(self.args.MyUserName +'\n'+self.args.VactimUser +'\n'+self.args.VactimUser +'\n')
-                                          
+                        MixUserName = List.write(self.args.MyUserName +'\n'+self.args.VactimUser +'\n'+self.args.VactimUser +'\n')                                         
           count = 0 
           Num1  = 1
           Num2  = 2
@@ -34,10 +35,10 @@ class Gen_List:
               with open (self.args.passordlist,'r') as readpass :
                     PassowordList = readpass.readlines()
                     PassNumber = int(self.args.Repete/3)
-              with open (path,'w') as finsh :
+              with open (self.path,'w') as finsh :
                    MixPassList = finsh.write(self.args.Mypassord+'\n'+str(PassowordList[0])+str(PassowordList[0]))    
               for Pass in  PassowordList :
-                with open (path,'a') as finsh :
+                with open (self.path,'a') as finsh :
                    MixPassList = finsh.write(self.args.Mypassord+'\n'+str(PassowordList[Num1])+ str(PassowordList[Num2]) )
                    count +=1
                    Num1 = Num1+2 
@@ -48,17 +49,43 @@ class Gen_List:
                  print('[!] Error : ', A )
                  exit()                                  
           except IndexError :
-              with open (path,'a') as finsh :
+              with open (self.path,'a') as finsh :
                  MixPassList = finsh.write(self.args.Mypassord+'\n'+str(PassowordList[-2])+str(PassowordList[-1] ) )                                       
           print('[+] MixList generate the Mix UserName Done ')
           print('[+] MixList generate the Mix Passoword List  Done ')
-          exit()
-     def control(self):
-        
+          if  not self.args.Json:
+              exit()
+     def JSON_FORMAT(self):
+         try:
+             if self.args.Json and self.args.File and\
+             self.args.File == self.args.Outlist : 
+                   with open (self.path2,'r')as readFile:
+                        Json_F = str(readFile.readlines()).replace('\n','').replace('\\n','')
+                        Json_format = json.dumps(Json_F)
+                        with open(self.path2+".json" ,'w') as jwrite:
+                             jwrite.write(Json_format)  
+                   with open (self.path,'r')as readFile:
+                        Json_F = str(readFile.readlines()).replace('\n','').replace('\\n','')
+                        Json_format = json.dumps(Json_F)
+                        with open(self.path+".json" ,'w') as jwrite:
+                             jwrite.write(Json_format)
+             elif  self.args.Json and self.args.File and\
+             self.args.File != self.args.Outlist :
+                   with open (self.args.File,'r')as readFile:
+                        Json_F = str(readFile.readlines()).replace('\n','').replace('\\n','')
+                        Json_format = json.dumps(Json_F)
+                        with open(self.args.File+".json" ,'w') as jwrite:
+                             jwrite.write(Json_format) 
+         except FileNotFoundError  as A :  
+                 print('[!] Error : ', A )
+                 exit()                                                                                          
+     def control(self):        
         parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]")      
         parser.add_argument("-VU  ",'--VactimUser'  , action=None    ,required=True          ,help =" add victim username to Create UserName List") 
         parser.add_argument("-MU  ","--MyUserName"  , action=None    ,required=True          ,help =" add your real  account UserName To mix with Victam UserName") 
-        parser.add_argument("-W   ","--passordlist" , action=None    ,required=True          ,help =" Path of the Oragnail Password List")
+        parser.add_argument("-W   ","--passordlist" , action=None    ,required=True          ,help =" self.path of the Oragnail Password List")
+        parser.add_argument("-J   ","--Json"        , action='store_true',required=False     ,help =" self.path of the Oragnail Password List")
+        parser.add_argument("-F   ","--File"        , action=None    ,required=False          ,help =" the file do you like to be jsoin format")
         parser.add_argument("-R   ","--Repete"      , action=None    ,required=True ,type=int,help =" Length Of the UserName List And Password List To generate")
         parser.add_argument("-MP  ","--Mypassord"   , action=None    ,required=True          ,help =" add Your real acount Password To can login after every two Time rowang Try ") 
         parser.add_argument("-O   ","--Outlist"     , action=None    ,required=True          ,help =" add the file name for New lists will generate ")
